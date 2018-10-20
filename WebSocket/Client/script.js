2e9182c6cc3b95a59e51a806cbd9dbfcf2b2324b
Fabiano Girardi <girardi.fabiano@gmail.com>
2018-10-20 06:01:21 -0300

    Example websocket

---
+++ WebSocket/Client/script.js
@@ -0,0 +1,14 @@
+const url = 'localhost:8080'
+const connection = new WebSocket(url)
+
+connection.onopen = () => {
+  connection.send('hey') 
+}
+
+connection.onerror = (error) => {
+  console.log(`WebSocket error: ${error}`)
+}
+
+connection.onmessage = (e) => {
+  console.log(e.data)
+}
