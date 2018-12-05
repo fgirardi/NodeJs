1d14d8cb9667d77c8de0e34dddd46afaa226ddf4
Fabiano Girardi <fgirardi@Fabianos-MacBook-Pro.local>
2018-12-05 07:55:53 -0200

    bugfix

--- promises/example.js
+++ promises/example.js
@@ -1,15 +1,14 @@
 let promise1 = new Promise((resolve, reject) => {
 	setTimeout(() => {
-		resolve("A promise 1 foi resolvida');
+		resolve('A promise 1 foi resolvida');
 	}, 2000);
 });
 
 let promise2 = new Promise((resolve, reject) => {
 	setTimeout(() => {
-		rejetc("A promise 2 foi rejeitada');
+		rejetc('A promise 2 foi rejeitada');
 	}, 2000);
 });
-
 
 promise1.then((resultado) => {
 	console.log(resultado);
