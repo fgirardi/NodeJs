c115ff5b717da8c39ff33551039022283657e78b
Fabiano Girardi <fgirardi@Fabianos-MacBook-Pro.local>
2018-12-05 07:51:21 -0200

    Example promise resolv and reject

--- promises/example.js
+++ promises/example.js
@@ -4,7 +4,19 @@ let promise1 = new Promise((resolve, reject) => {
 	}, 2000);
 });
 
+let promise2 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		rejetc("A promise 2 foi rejeitada');
+	}, 2000);
+});
+
 
 promise1.then((resultado) => {
 	console.log(resultado);
 })
+
+promise2.then((resultado) => {
+	console.log(resultado);
+}).catch((error) => {
+	console.error(error);
+});
