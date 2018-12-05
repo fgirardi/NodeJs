3eb987caa93dd161db74358eab2ca59f8d22684a
Fabiano Girardi <girardi.fabiano@gmail.com>
2018-12-05 20:51:16 -0200

    Example SyncAwait

--- promises/ExampleSyncAwait.js
+++ promises/ExampleSyncAwait.js
@@ -3,9 +3,34 @@
 //also Async await is less verbose to read.
 //Example:
 async function main() {
-	if (await getResultado(5) < 10) {
-		console.log('O valor que retornou eh maior que 10');
+	//Using await
+	try{
+		if (await getResultado(5) < 10) {
+			console.log('O valor que retornou eh maior que 10');
+			if (await getResultado(3) < 10) {
+				console.log('O valor que retornou eh menor que 10');
+			}
+		}
+	} catch(err) {
+		console.log('Error');
+		console.log(err);
 	}
+	
+	//Normal Promise
+	//getResultado(5)
+	//.then((resultado) => {
+		//console.log('O valor que retornou eh maior que 10');
+		//getResultado(3)
+		//.then((resultado) => {
+			//console.log('O valor que retornou eh menor que 10');
+		//}).catch((erro) => {
+			//console.log('Error 3');
+			//console.log(error);
+		//})
+	//}).catch((erro) => {
+		//console.log("Error 5");
+		//console.log(error);
+	//})
 }
 main();
 
