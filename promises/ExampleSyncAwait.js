e288a67d0f940ea7b3020c78fdf95cc80363267e
Fabiano Girardi <girardi.fabiano@gmail.com>
2018-12-05 20:32:07 -0200

    Example SyncAwait

---
+++ promises/ExampleSyncAwait.js
@@ -0,0 +1,9 @@
+//Example using Async await X Promisse.
+//Using Async await is more easy to understand the source code.
+//also Async await is less verbose to read.
+//Example:
+
+
+function getResultado(parametro) {
+	return new Promise((resolve, reject) => {
+	});
