9e1d687cc9ea5bc3dcb937c2e164d0ae81e14bd2
Fabiano Girardi <fabiano@debianFG.GIRARDI_NET>
2019-02-03 12:59:35 -0500

    Example upload File

--- UploadFiles/app.js
+++ UploadFiles/app.js
@@ -4,4 +4,38 @@ var express = require("express"),
 	upload = require("express-fileupload");
 app.use(upload())
 
+console.log("Server Starded :)")
+
+app.get("/",function(req, res) {
+
+	res.sendFile(__dirname + "index.html");
+
+
+})
+
+app.post("/", function(req, res) {
+
+	if (req.files) {
+	
+		var file = req.files.filename;
+		var filename = req.files.name;
+
+		file.mv("./upload" + filename, function(err) {
+		
+			if(err) {
+				
+				console.log(err);
+				res.send("Error occured");
+			
+			} else {
+			
+				res.send("Done");
+
+			}
+
+		})
+	}
+
+})
+
 
