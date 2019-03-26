d7be313bb7952a5a9a2308d71e6d17faf44fd4eb
Fabiano Girardi <fgirardi@Anonymous-MB.local>
2019-03-26 08:23:07 -0300

    WIP

--- CadastroLivros/server.js
+++ CadastroLivros/server.js
@@ -1,5 +1,15 @@
 const http = require('http');
-const servidor = http.createServer();
+const servidor = http.createServer(function (req, res) {
+	res.end(`<html>
+			<head>
+				<meta charset ="utf-8">
+			</head>
+			<body>
+				<h1>Livraria do Girardi</h1>
+			</body>
+		</html>`
+		);
+});
 servidor.listen(3000);
 
 
